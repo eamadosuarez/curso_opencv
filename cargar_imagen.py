d388f182fe55c50cd7d7016d094e06509b7ad43a
eamadosuarez <eamado.suarez@gmail.com>
2022-06-20 15:27:56 -0500

    Escritura de nueva imagen

--- cargar_imagen.py
+++ cargar_imagen.py
@@ -1,7 +1,12 @@
+#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
 import cv2;
 
 ruta = '/home/napoleon/codigo/curso_opencv/imagenes/tux.jpg';
 imagen = cv2.imread(ruta);
-cv2.imshow('Copy', imagen)
+ruta_copia = '/home/napoleon/codigo/curso_opencv/imagenes/tux_copia.jpg';
+cv2.imwrite(ruta_copia,imagen);
+imagen_copia = cv2.imread(ruta_copia)
+cv2.imshow('Original', imagen)
+cv2.imshow('Copia', imagen_copia)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
